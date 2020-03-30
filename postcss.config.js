b9a99ccb9a0dff8a32198afb0cbe1377b93be5a0
Bryce Wray <bwray@wraytx.com>
2020-03-30 12:52:10 -0500

    Adding PostCSS config file

---
+++ postcss.config.js
@@ -0,0 +1,30 @@
+const postcssPresetEnv = require('postcss-preset-env')
+const cssnano = require('cssnano')
+const cssvariables = require('postcss-css-variables')
+const postcssPartialImport = require ('postcss-partial-import')
+const postcssNesting = require('postcss-nesting')
+
+module.exports = {
+  plugins: [
+    require('postcss-preset-env'),
+    postcssPresetEnv({
+      stage: 0,
+      browsers: [
+        'last 2 versions',
+        'IE 11',
+      ],
+      features: {
+        'nesting-rules': true,
+        'custom-properties': true,
+      },
+    }),
+    postcssPartialImport({
+      root: './src/assets/css/',
+    }),
+    cssvariables({}),
+    postcssNesting({}),
+    process.env.NODE_ENV === 'production' 
+    ? cssnano({ preset: 'default' })
+    : null,
+  ],
+}
