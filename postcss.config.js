53097139de22432df74c7907901d648edbfc0d9d
Bryce Wray <bwray@wraytx.com>
2020-05-24 19:54:48 -0500

    Config

--- postcss.config.js
+++ postcss.config.js
@@ -1,31 +1,13 @@
-const postcssPresetEnv = require('postcss-preset-env')
-const cssnano = require('cssnano')
-const cssvariables = require('postcss-css-variables')
-const postcssPartialImport = require ('postcss-partial-import')
+const postcssImport = require('postcss-import')
 const postcssNesting = require('postcss-nesting')
 
 module.exports = {
   plugins: [
-    require('postcss-preset-env'),
-    postcssPresetEnv({
-      stage: 0,
-      browsers: [
-        'last 2 versions',
-        'IE 11',
-      ],
-      features: {
-        'nesting-rules': true,
-        'custom-properties': true,
-      },
+    require('postcss-preset-env')({ stage: 1 }),
+    postcssImport({
+      path: 'assets/css',
     }),
-    postcssPartialImport({
-      path: 'assets/css/',
-      // see https://discourse.gohugo.io/t/pipes-postcss-postcss-import-how-to-keep-filesystem-context/15164
-    }),
-    cssvariables({}),
+    require('tailwindcss'),
     postcssNesting({}),
-    process.env.HUGO_ENVIRONMENT === 'production' 
-    ? cssnano({ preset: 'default' })
-    : null,
   ],
 }
