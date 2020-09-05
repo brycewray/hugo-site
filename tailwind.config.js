9dbc37c48e41014352e7851fafcca16bb4918e1f
Bryce Wray <bwray@wraytx.com>
2020-09-05 16:24:53 -0500

    Bringing config up to date

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
   purge: {
     content: [
       './layouts/**/*.html',
     ],
   },
   theme: {
+    backgroundImage: {
+      'gradient-to-t': 'linear-gradient(to top, var(--gradient-color-stops))',
+      'gradient-to-tr': 'linear-gradient(to top right, var(--gradient-color-stops))',
+      'gradient-to-r': 'linear-gradient(to right, var(--gradient-color-stops))',
+      'gradient-to-br': 'linear-gradient(to bottom right, var(--gradient-color-stops))',
+      'gradient-to-b': 'linear-gradient(to bottom, var(--gradient-color-stops))',
+      'gradient-to-bl': 'linear-gradient(to bottom left, var(--gradient-color-stops))',
+      'gradient-to-l': 'linear-gradient(to left, var(--gradient-color-stops))',
+      'gradient-to-tl': 'linear-gradient(to top left, var(--gradient-color-stops))',
+    },
+    gradientColorStops: (theme) => theme('colors'),
     screens: {
       'sm': '640px',  // => @media (min-width: 640px) { ... }
       'md': '768px',  // => @media (min-width: 768px) { ... }
@@ -16,6 +31,10 @@ module.exports = {
         'dark': {
           'raw': '(prefers-color-scheme: dark)',
         },
+      },
+      fontSize: { // these will cease to be relevant with TWCSS 2.0, which will add 7xl, 8xl, **and** 9xl
+        '7xl': '4.5rem',
+        '8xl': '5rem',
       },
       colors: {
         blue: {
