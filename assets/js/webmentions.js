f654e91d30656f31848bf64a940e7aa2dc2eacb7
Bryce Wray <bwray@wraytx.com>
2020-09-08 07:44:30 -0500

    Less chatty on build

--- assets/js/webmentions.js
+++ assets/js/webmentions.js
@@ -35,14 +35,14 @@ const processMentionsJson = (data) => {
     urlDataItem[wmProperty].push(item)
   });
 
-  console.log(urlData)
+  // console.log(urlData)
 
   // For each URL in the blog we now have a JSON stucture that has all the like, mentions and reposts
   if(fs.existsSync('./data') === false) fs.mkdirSync('./data')
   Object.keys(urlData).forEach(key => {
     const item = urlData[key]
     const md5url = md5(key)
-    console.log(key, md5url)
+    // console.log(key, md5url)
     fs.writeFileSync(`./data/${md5url}.json`, JSON.stringify(item))
   })
 }
