1fefe19d07ac3da780ec3985c64af000a916e28a
Bryce Wray <bwray@wraytx.com>
2020-10-18 09:35:30 -0500

    Preparing for TWCSS theme and... ...sep. PostCSS config files via Hugo Pipes

---
+++ themes/ofotipostcss/assets/postcss.config.js
@@ -0,0 +1,14 @@
+const postcssImport = require('postcss-import')
+const postcssNesting = require('postcss-nesting')
+const postcssClean = require('postcss-clean')
+
+module.exports = {
+  plugins: [
+    require('postcss-preset-env')({ stage: 1 }),
+    postcssImport({
+      path: 'assets/css',
+    }),
+    postcssNesting({}),
+    postcssClean({})
+  ],
+}
