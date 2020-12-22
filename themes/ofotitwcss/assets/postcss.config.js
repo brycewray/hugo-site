98e95ca14461c447b31eaf19afdaf9053e489509
Bryce Wray <bwray@wraytx.com>
2020-12-22 13:09:08 -0600

    More std. PostCSS/TWCSS 2.x setup

--- themes/ofotitwcss/assets/postcss.config.js
+++ themes/ofotitwcss/assets/postcss.config.js
@@ -1,15 +1,10 @@
-const postcssImport = require('postcss-import')
-const postcssNesting = require('postcss-nesting')
-const postcssClean = require('postcss-clean')
-
 module.exports = {
   plugins: [
-    require('postcss-preset-env')({ stage: 1 }),
-    postcssImport({
-      path: 'assets/css',
+    require('postcss-import')({
+      path: ['themes/ofotitwcss/assets/css']
     }),
     require('tailwindcss'),
-    postcssNesting({}),
-    postcssClean({})
+    require('postcss-preset-env')({ stage: 1 }),
+    require('postcss-clean'),
   ],
 }
