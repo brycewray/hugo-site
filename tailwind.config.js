655e98768799a75f1e7e2ca90fc7cb9cf48f8b11
Bryce Wray <bwray@wraytx.com>
2020-12-22 15:50:56 -0600

    CSS/config chgs, mostly for Inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,9 @@ module.exports = {
         '900': '#000066',
       },
     },
+    fontFamily: {
+      'sans': ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji',],
+    }
   },
   variants: {},
   plugins: [], // if we add forms, do it here
