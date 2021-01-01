8ac4a7496bca6782b562c12be7ddcc69a148ed94
Bryce Wray <bwray@wraytx.com>
2021-01-01 04:23:48 -0600

    Move to"themeless" TWCSS-only setup

--- themes/ofotipostcss/assets/postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: [
-    require('postcss-import')({
-      path: ['themes/ofotipostcss/assets/css']
-    }),
-    require('postcss-preset-env')({ stage: 1 }),
-    require('postcss-clean'),
-  ],
-}
