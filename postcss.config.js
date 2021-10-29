187ba09eb6e4acc5e69a76e66d6a60a620068a8a
Bryce Wray <bwray@wraytx.com>
2021-10-28 23:27:06 -0500

    Whoops - fixed

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,11 @@
-{
-  "devDependencies": {
-    "autoprefixer": "^10.4.0",
-    "postcss": "^8.3.11",
-    "postcss-cli": "^9.0.1"
-  }
+module.exports = {
+  plugins: [
+    require("autoprefixer")({
+      browsers: [
+        "last 4 versions",
+        "> 1%",
+        "maintained node versions"
+      ]
+    })
+  ]
 }
