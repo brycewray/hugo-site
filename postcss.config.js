cfc0cefe56ec25838e0c41e48fd94829a7a76781
Bryce Wray <bwray@wraytx.com>
2021-11-22 12:36:48 -0600

    Config changes for SCSS setup

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,11 @@
-let hugoTheme = 'twjit-thoreau' // choices are twjit-hero and twjit-thoreau
-
 module.exports = {
   plugins: [
-    require('postcss-import')({
-      path: [`themes/${hugoTheme}/assets/css`]
-    }),
-    require('tailwindcss/nesting'),
-    require("autoprefixer"),
+    require("autoprefixer")({
+      browsers: [
+        "last 4 versions",
+        "> 1%",
+        "maintained node versions"
+      ]
+    })
   ]
 }
