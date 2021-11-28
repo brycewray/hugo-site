2d6bf1fa398223870a1fdb4ee1d56e1a179126c4
Bryce Wray <bwray@wraytx.com>
2021-11-28 09:16:45 -0600

    Going themeless

--- assets/js/lazyload-helper.esm.js
+++ assets/js/lazyload-helper.esm.js
@@ -1,7 +1,7 @@
 // runs in the base template after lazyload itself;
 // mitigates CSP's refusing to load inline script
 
-import LazyLoad from './themes/scss-thoreau/assets/js/lazyload_17-5-0.esm.min.js'
+import LazyLoad from './assets/js/lazyload_17-5-0.esm.min.js'
 
 var lazyLoadInstance = new LazyLoad({
   threshold: 150,
