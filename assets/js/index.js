3017c76ad09d3866f88298cdb1af7a947f06bb0e
Bryce Wray <bwray@wraytx.com>
2021-11-29 16:44:50 -0600

    More accurate naming (maybe)

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,14 +2,13 @@
 // mitigates CSP's refusing to load inline script
 
 import LazyLoad from './assets/js/lazyload_17-5-0.esm.js'
-import flyingPages from './assets/js/flying-pages_2-1-2.esm.js'
+import flyingPages from './assets/js/flying-pages_2-1-2.mod.js'
 
 var lazyLoadInstance = new LazyLoad({
   threshold: 150,
 })
 
 window.FPConfig = {
-  delay: 2, // default = 0
   ignoreKeywords: [
     'index.xml',
     'index.json',
@@ -19,8 +18,6 @@ window.FPConfig = {
     '/sitemap',
     '/sitemap.xml'
   ],
-  maxRPS: 3, // default = 3
-  hoverDelay: 200, // default = 50
 }
 
 flyingPages()
