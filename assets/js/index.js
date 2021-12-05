21df69cafbe68a66e0c0e4d362bb1402d1f7f35d
Bryce Wray <bwray@wraytx.com>
2021-12-05 10:31:36 -0600

    Removing Instant.Page at least for now

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,5 @@
-// runs in the base template after lazyload itself;
-// mitigates CSP's refusing to load inline script
-
 import LazyLoad from './assets/js/lazyload_17-5-0.esm.js'
-import './assets/js/instantpage_5-1-0.js'
+// import './assets/js/instantpage_5-1-0.js'
 
 var lazyLoadInstance = new LazyLoad({
   threshold: 150,
