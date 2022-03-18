808db98bb96a570668fce7ce198cd1a9a1c672a5
Bryce Wray <bwray@wraytx.com>
2022-03-18 16:49:30 -0500

    Work on code blocks

---
+++ assets/js/codeblocks_2019-10-21.js
@@ -0,0 +1,19 @@
+/*
+shamelessly stolen from https://github.com/gohugoio/hugoDocs/blob/master/_vendor/github.com/gohugoio/gohugoioTheme/assets/js/codeblocks.js as of 2021-03-18
+*/
+
+let article = document.getElementById('postArticle')
+
+if (article) {
+  let codeBlocks = article.getElementsByTagName('code')
+    for (let [key, codeBlock] of Object.entries(codeBlocks)){
+      var widthDif = codeBlock.scrollWidth - codeBlock.clientWidth
+      if (widthDif > 0) {
+        codeBlock.parentNode.classList.add('expand')
+        console.log(key, " scrollWidth = ", codeBlock.scrollWidth)
+        console.log("    clientWidth = ", codeBlock.clientWidth)
+        console.log("    offsetWidth = ", codeBlock.offsetWidth)
+        console.log("    widthDif    = ", widthDif)
+      }
+  }
+}
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,1 @@
-import LazyLoad from './assets/js/lazyload_17-5-0.esm.js'
-// import './assets/js/instantpage_5-1-0.js'
-
-var lazyLoadInstance = new LazyLoad({
-  threshold: 150,
-})
+import './assets/js/codeblocks_2019-10-21.js'
