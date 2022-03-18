c0edc239ae13fc90d5a64480b4abd133d9a0e0d5
Bryce Wray <bwray@wraytx.com>
2022-03-18 17:03:15 -0500

    Back to where we were (apparently OK as-is)

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,6 @@
-import './assets/js/codeblocks_2019-10-21.js'
+import LazyLoad from './assets/js/lazyload_17-5-0.esm.js'
+// import './assets/js/instantpage_5-1-0.js'
+
+var lazyLoadInstance = new LazyLoad({
+  threshold: 150,
+})
