32cf66b97c6e221ea05846591f9b479e93aa14a8
Bryce Wray <bwray@wraytx.com>
2022-04-14 10:05:26 -0500

    Back to where we were

--- build-to-vercel.sh
+++ build-to-vercel.sh
@@ -3,11 +3,10 @@
 # based on https://discourse.gohugo.io/t/using-dart-sass-hugo-and-netlify/37099/7
 # - thanks, @bep!
 
+echo "Install Dart Sass Embedded..."
+
 # This is in Vercel's PATH.
 BIN_DIR=${pwd}/bin
-
-# first, install Embedded Dart Sass
-echo "Install Embedded Dart Sass..."
 
 DARTSASS_VERSION=1.50.0
 
@@ -23,22 +22,6 @@ rm -rf sass_embedded*;
 
 dart-sass-embedded --version
 
-# now install Hugo
-
-echo "Install Hugo extended version..."
-
-HUGO_VERSION=0.97.0
-
-curl -LJO https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_macOS-64bit.tar.gz;
-
-tar -xvzf hugo_extended_${HUGO_VERSION}_macOS-64bit.tar.gz;
-
-mv hugo /usr/local/bin/
-
-which hugo
-
-hugo version
-
-echo "Building site..."
+echo "Building..."
 
 hugo --gc --minify
