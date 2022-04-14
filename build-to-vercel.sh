b7644dae8653081955466085f61f39f92d6f5ad2
Bryce Wray <bwray@wraytx.com>
2022-04-14 09:48:38 -0500

    Don't need dir name; binary is extracted

--- build-to-vercel.sh
+++ build-to-vercel.sh
@@ -29,17 +29,13 @@ echo "Install Hugo extended version..."
 
 HUGO_VERSION=0.97.0
 
-mkdir -p $BIN_DIR
-
 # https://github.com/gohugoio/hugo/releases/download/v0.97.0/hugo_extended_0.97.0_macOS-64bit.tar.gz
 
 curl -LJO https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_macOS-64bit.tar.gz;
 
 tar -xvf hugo_extended_${HUGO_VERSION}_macOS-64bit.tar.gz;
 
-mv hugo_extended_${HUGO_VERSION}_macOS-64bit/hugo $BIN_DIR
-
-rm -rf hugo_extended*;
+mv hugo $BIN_DIR
 
 hugo version
 
