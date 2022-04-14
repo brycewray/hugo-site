409412dfdb4b58e24d64d7bd4b7b7bd525a3dab2
Bryce Wray <bwray@wraytx.com>
2022-04-14 09:15:54 -0500

    (Experiment) Adding Hugo version to build script

--- build-to-vercel.sh
+++ build-to-vercel.sh
@@ -3,10 +3,11 @@
 # based on https://discourse.gohugo.io/t/using-dart-sass-hugo-and-netlify/37099/7
 # - thanks, @bep!
 
-echo "Install Dart Sass Embedded..."
-
 # This is in Vercel's PATH.
 BIN_DIR=${pwd}/bin
+
+# first, install Embedded Dart Sass
+echo "Install Embedded Dart Sass..."
 
 DARTSASS_VERSION=1.50.0
 
@@ -22,6 +23,24 @@ rm -rf sass_embedded*;
 
 dart-sass-embedded --version
 
-echo "Building..."
+# now install Hugo
+
+echo "Install Hugo extended version..."
+
+HUGO_VERSION=0.97.0
+
+# https://github.com/gohugoio/hugo/releases/download/v0.97.0/hugo_extended_0.97.0_macOS-64bit.tar.gz
+
+curl -LJO https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_macOS-64bit.tar.gz;
+
+tar -xvf hugo_extended_${HUGO_VERSION}_macOS-64bit.tar.gz;
+
+mv hugo $BIN_DIR
+
+rm -rf hugo_extended*;
+
+hugo --version
+
+echo "Building site..."
 
 hugo --gc --minify
