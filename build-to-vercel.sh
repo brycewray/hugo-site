05ac3486299b88a3e3c790507b8371ea9c98b070
Bryce Wray <bwray@wraytx.com>
2022-04-14 09:43:44 -0500

    Dealing with `mv` issue

--- build-to-vercel.sh
+++ build-to-vercel.sh
@@ -29,6 +29,8 @@ echo "Install Hugo extended version..."
 
 HUGO_VERSION=0.97.0
 
+mkdir -p $BIN_DIR
+
 # https://github.com/gohugoio/hugo/releases/download/v0.97.0/hugo_extended_0.97.0_macOS-64bit.tar.gz
 
 curl -LJO https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_macOS-64bit.tar.gz;
