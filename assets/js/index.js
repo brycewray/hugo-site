6cc35870fefa1a99b0b5dd9e835175496d0a471c
Bryce Wray <bwray@wraytx.com>
2022-10-10 09:49:39 -0500

    Give Flying Pages another try

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,16 @@
-import LazyLoad from './assets/js/lazyload_17-5-0.esm.js'
-// import './assets/js/instantpage_5-1-0.js'
+import flyingPages from './assets/js/flying-pages_2-1-2.mod.js'
 
-var lazyLoadInstance = new LazyLoad({
-	threshold: 150,
-})
+window.FPConfig = {
+  ignoreKeywords: [
+    'index.xml',
+    'index.json',
+    '/about',
+    '/privacy',
+    '/contact',
+		'/search',
+    '/sitemap',
+    '/sitemap.xml'
+  ],
+}
+
+flyingPages()
