4bad345dd0a41784bc05a60d34eea27ad68b26b5
Bryce Wray <bwray@wraytx.com>
2023-02-09 10:43:10 -0600

    Keep code local (avoid CF cookies)

--- assets/js/copy-code-button.js
+++ assets/js/copy-code-button.js
@@ -41,7 +41,7 @@ if (navigator && navigator.clipboard) {
 } else {
 	const script = document.createElement("script");
 	script.src =
-		"https://cdnjs.cloudflare.com/ajax/libs/clipboard-polyfill/2.7.0/clipboard-polyfill.promise.js";
+		"/assets/js/clipboard-polyfill/2.7.0/clipboard-polyfill.promise.js";
 	script.integrity = "sha256-waClS2re9NUbXRsryKoof+F9qc1gjjIhc2eT7ZbIv94=";
 	script.crossOrigin = "anonymous";
 	script.onload = () => addCopyButtons(clipboard);
