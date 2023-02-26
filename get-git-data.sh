dd46f18f14a8ca8c402873cd81f1b0610d00dbd7
Bryce Wray <bwray@wraytx.com>
2023-02-26 12:55:09 -0600

    Trying to `fetch-depth`-proof host GUIs

---
+++ get-git-data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# **MUST** be run **before** Hugo builds if possible
+rm -rf data/gitoutput.yml # avoid appending to existing
+printf "gitinfo:\n" >> data/gitoutput.yml
+cd content
+git ls-tree -r --name-only HEAD | while read filename; do
+  printf "\055 FilePath: $filename\n$(git log -1 --all --pretty=format:"  Hash: %H\n  AbbreviatedHash: %h\n  LastmodDate: %cI" -- $filename)\n" >> ../data/gitoutput.yml
+done
