ee17ad938aadc22f182791bfb950b51ffba555e2
Bryce Wray <bwray@wraytx.com>
2023-02-26 10:54:56 -0600

    Revert to previous

--- get-git-data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# **MUST** be run **before** Hugo builds if possible
-rm -rf data/gitoutput.yml # avoid appending to existing
-printf "gitinfo:\n" >> data/gitoutput.yml
-cd content
-git ls-tree -r --name-only HEAD | while read filename; do
-  printf "\055 $filename\n$(git log -1 --pretty=format:"  - %H\n  - %h" -- $filename)\n" >> ../data/gitoutput.yml
-done
