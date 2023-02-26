bdeb2ec3db52a4ec1391bbe605dd5ee05b2d81a7
Bryce Wray <bwray@wraytx.com>
2023-02-26 13:52:41 -0600

    Admit defeat (can't overcome V shallow clone)

--- get-git-data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# **MUST** be run **before** Hugo builds if possible
-# rm -rf data/gitoutput.yml # avoid appending to existing
-printf "gitinfo:\n" >> data/gitoutput.yml
-cd content
-git ls-tree -r --name-only HEAD | while read filename; do
-  printf "\055 FilePath: $filename\n$(git log -1 --all --pretty=format:"  Hash: %H\n  AbbreviatedHash: %h\n  LastmodDate: %cI" -- $filename)\n" >> ../data/gitoutput.yml &&
-  printf "\055 FilePath: $filename\n$(git log -1 --all --pretty=format:"  Hash: %H\n  AbbreviatedHash: %h\n  LastmodDate: %cI" -- $filename)\n"
-done
