ebac0e54c63621a2e40056573dd37c4ba972e467
Bryce Wray <bwray@wraytx.com>
2023-02-26 09:32:34 -0600

    For another V GUI test

---
+++ get-git-data.sh
@@ -0,0 +1,6 @@
+rm -rf data/gitoutput.yml # avoid appending to existing
+printf "gitinfo:\n" >> data/gitoutput.yml
+cd content
+git ls-tree -r --name-only HEAD | while read filename; do
+  printf "\055 $filename\n$(git log -1 --pretty=format:"  - %H\n  - %h" -- $filename)\n" >> ../data/gitoutput.yml
+done
