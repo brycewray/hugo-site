1de518b51f593dee472f35ed9566422495b9862e
Bryce Wray <bwray@wraytx.com>
2023-02-26 13:22:53 -0600

    Try without the `rm -rf` part

--- get-git-data.sh
+++ get-git-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # **MUST** be run **before** Hugo builds if possible
-rm -rf data/gitoutput.yml # avoid appending to existing
+# rm -rf data/gitoutput.yml # avoid appending to existing
 printf "gitinfo:\n" >> data/gitoutput.yml
 cd content
 git ls-tree -r --name-only HEAD | while read filename; do
