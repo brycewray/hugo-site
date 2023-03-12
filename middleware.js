fb60562f41e8a97568b53780bef241e62478b977
Bryce Wray <bwray@wraytx.com>
2023-03-11 22:12:52 -0600

    Bring back img headers?

--- middleware.js
+++ middleware.js
@@ -15,6 +15,28 @@ function generateNonce() {
 export default async function handleRequest(request) {
 	const nonce = generateNonce()
 	let response = await fetch(request)
+
+  let imageResponse = await fetch(request)
+  let type = imageResponse.headers.get("Content-Type") || ""
+  if (!type.startsWith("text/")) {
+    // Not text. Don't modify.
+    let newHeaders = new Headers(imageResponse.headers)
+    newHeaders.set("Cache-Control", "public, max-age=2678400, immutable")
+    newHeaders.set("CDN-Cache-Control", "public, max-age=2678400, immutable")
+    newHeaders.set("x-BW-test", "Non-text item - headers edited!")
+    // newHeaders.set("Permissions-Policy", "interest-cohort=()")
+    newHeaders.set("Strict-Transport-Security", "max-age=63072000; includeSubDomains; preload")
+    newHeaders.set("X-Frame-Options", "SAMEORIGIN")
+    newHeaders.set("X-Content-Type-Options", "nosniff")
+    newHeaders.set("Referrer-Policy", "no-referrer, strict-origin-when-cross-origin")
+    newHeaders.set("cf-nonce-generator", "HIT")
+    return new Response(imageResponse.body, {
+      status: imageResponse.status,
+      statusText: imageResponse.statusText,
+      headers: newHeaders
+    })
+  }
+
   const html = (await response.text())
     .replace(/DhcnhD3khTMePgXw/gi, nonce)
     .replace(
@@ -47,9 +69,26 @@ export default async function handleRequest(request) {
       `nonce="${nonce}" src="/assets/js/lite-yt-embed_`
     )
     .replace(/<style/g, `<style nonce="${nonce}"`)
+	let url = new URL(request.url)
+	let jsStuff = false
+	let svgStuff = false
+  const jsRegex = /(.*\.(js))$/
+  const svgRegex = /(.*\.(svg))$/
+  if (url.pathname.match(jsRegex)) {
+    jsStuff = true
+  }
+  if (url.pathname.match(svgRegex)) {
+    svgStuff = true
+  }
 
 	let newHeaders = new Headers(response.headers)
 	newHeaders.set("Content-Security-Policy", `base-uri 'self' https://*.brycewray.com; connect-src 'self' https://*.brycewray.com https://*.cloudinary.com https://*.cloudflareinsights.com https://cloudflareinsights.com https://*.ytimg.com https://*.ggpht.com https://*.youtube-nocookie.com https://*.fosstodon.org https://*.mastodon.social https://*.mstdn.social; default-src 'self'; frame-ancestors 'self' https://*.brycewray.com https://*.youtube-nocookie.com; font-src 'self' https://*.brycewray.com https://*.gstatic.com data:; form-action 'self' https://*.duckduckgo.com https://duckduckgo.com; frame-src 'self' https://*.brycewray.com https://*.youtube-nocookie.com; img-src 'self' https://*.brycewray.com https://*.cloudinary.com https://*.ytimg.com https://*.ggpht.com https://*.youtube-nocookie.com https://*.gstatic.com https://*.twimg.com https://*.amazonaws.com https://*.fosstodon.org https://*.mastodon.social https://*.mstdn.social https://*.google.com https://translate.googleapis.com data:; media-src 'self' https://*.brycewray.com https://*.cloudinary.com https://*.ytimg.com https://*.ggpht.com https://*.youtube-nocookie.com https://*.gstatic.com https://*.twimg.com https://*.fosstodon.org https://*.mastodon.social https://*.mstdn.social data:; object-src 'none'; script-src 'self' 'nonce-${nonce}' 'unsafe-eval'; script-src-elem 'self' 'nonce-${nonce}'; style-src 'self' https://*.brycewray.com https://*.youtube-nocookie.com data: https://*.google.com https://translate.googleapis.com 'nonce-${nonce}'; report-uri https://brycewray.report-uri.com/r/d/csp/reportOnly;`)
+  if (jsStuff) {
+    newHeaders.set("Content-Type", "application/javascript; charset=utf-8")
+  }
+  if (svgStuff) {
+    newHeaders.set("Content-Type", "image/svg+xml; charset=utf-8")
+  }
 
 	return new Response(html, {
 		status: response.status,
