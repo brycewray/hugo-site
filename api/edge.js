b3af33b81a5ec5db620ca81574a41c99907c06c4
Bryce Wray <bwray@wraytx.com>
2023-03-11 20:26:10 -0600

    Try again with Edge F

--- api/edge.js
+++ api/edge.js
@@ -1,34 +1,7 @@
 export const config = {
-	runtime: 'edge',
-}
+  runtime: 'edge',
+};
 
-function dec2hex(dec) {
-  return ("0" + dec.toString(16)).substr(-2)
-}
-
-function generateNonce() {
-  const arr = new Uint8Array(12)
-  crypto.getRandomValues(arr)
-  const values = Array.from(arr, dec2hex)
-  return [
-    btoa(values.slice(0, 5).join("")).substr(0, 14),
-    btoa(values.slice(5).join("")),
-  ].join("/")
-}
-const nonce = generateNonce()
-
-export default async function handler(req) {
-	return new Response (
-		JSON.stringify({
-			message: 'Hello, world!',
-		}),
-		{
-			status: 200,
-			headers: {
-				'content-type': 'application/json',
-				'cache-control': 'public, s-maxage=1200, stale-while-revalidate=600',
-				'x-nonce-test': "'" + nonce + "'",
-			},
-		}
-	)
-}
+export default (req) => {
+  return new Response(`Hello, from ${req.url} I'm now an Edge Function!`);
+};
