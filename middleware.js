31c877b875a35243d0371dc8f7190bcab50ec6d1
Bryce Wray <bwray@wraytx.com>
2023-03-23 05:36:40 -0500

    Re-add nonce function only

--- middleware.js
+++ middleware.js
@@ -1,3 +1,17 @@
+function dec2hex(dec) {
+  return ("0" + dec.toString(16)).substr(-2)
+}
+
+function generateNonce() {
+  const arr = new Uint8Array(12)
+  crypto.getRandomValues(arr)
+  const values = Array.from(arr, dec2hex)
+  return [
+    btoa(values.slice(0, 5).join("")).substr(0, 14),
+    btoa(values.slice(5).join("")),
+  ].join("/")
+}
+
 export default async function handleRequest(request) {
 	try { // try-catch so a function crash doesn't crash the site
 		let response = await fetch(request)
