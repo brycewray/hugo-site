bacc74c555ff4bab3642001570663b5076f45551
Bryce Wray <bwray@wraytx.com>
2023-03-23 06:01:40 -0500

    With `replace`s again

--- middleware.js
+++ middleware.js
@@ -54,7 +54,6 @@ export default async function handleRequest(request) {
 			})
 		}
 
-/*
 		const html = (await response.text())
 			.replace(/DhcnhD3khTMePgXw/gi, nonce)
 			.replace(
@@ -75,7 +74,6 @@ export default async function handleRequest(request) {
 				`nonce="${nonce}" src="/assets/js/lite-yt-embed_`
 			)
 			.replace(/<style/g, `<style nonce="${nonce}"`)
-*/
 
 		let ttl = undefined
 		// let cache = caches.default
