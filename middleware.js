365b2cd67d557eb7a75d0c52fc07bf3e59fba5a0
Bryce Wray <bwray@wraytx.com>
2023-04-13 13:01:06 -0500

    [no ci] Revert - no crash, but no help w CFW delay

--- middleware.js
+++ middleware.js
@@ -33,6 +33,27 @@ export default async function handleRequest(request) {
 	try { // try-catch so a function crash doesn't crash the site
 		let response = await fetch(request)
 
+		let imageResponse = await fetch(request)
+		let type = imageResponse.headers.get("Content-Type") || ""
+		if (!type.startsWith("text/")) {
+			// Not text. Don't modify.
+			let newHeaders = new Headers(imageResponse.headers)
+			newHeaders.set("Cache-Control", "public, max-age=2678400, immutable")
+			// newHeaders.set("CDN-Cache-Control", "public, max-age=2678400, immutable")
+			// newHeaders.set("x-BW-test", "Non-text item - headers edited!")
+			// newHeaders.set("Permissions-Policy", "interest-cohort=()")
+			newHeaders.set("Strict-Transport-Security", "max-age=63072000; includeSubDomains; preload")
+			newHeaders.set("X-Frame-Options", "SAMEORIGIN")
+			newHeaders.set("X-Content-Type-Options", "nosniff")
+			newHeaders.set("Referrer-Policy", "no-referrer, strict-origin-when-cross-origin")
+			newHeaders.set("x-nonce-generator", "HIT")
+			return new Response(imageResponse.body, {
+				status: imageResponse.status,
+				statusText: imageResponse.statusText,
+				headers: newHeaders
+			})
+		}
+
 		const html = (await response.text())
 			.replace(/DhcnhD3khTMePgXw/gi, nonce)
 			.replace(
@@ -50,10 +71,50 @@ export default async function handleRequest(request) {
 			)
 			.replace(/<style/g, `<style nonce="${nonce}"`)
 
+		let ttl = undefined
+		// let cache = caches.default
+		let url = new URL(request.url)
+		let shouldCache = false
+		let jsStuff = false
+		let svgStuff = false
+
+		const filesRegex = /(.*\.(ac3|avi|bmp|br|bz2|css|cue|dat|doc|docx|dts|eot|exe|flv|gif|gz|ico|img|iso|jpeg|jpg|js|json|map|mkv|mp3|mp4|mpeg|mpg|ogg|pdf|png|ppt|pptx|qt|rar|rm|svg|swf|tar|tgz|ttf|txt|wav|webp|webm|webmanifest|woff|woff2|xls|xlsx|xml|zip))$/
+		const jsRegex = /(.*\.(js))$/
+		const svgRegex = /(.*\.(svg))$/
+
+		if (url.pathname.match(filesRegex)) {
+			shouldCache = true
+			ttl = 31536000
+		}
+		if (url.pathname.match(jsRegex)) {
+			jsStuff = true
+		}
+		if (url.pathname.match(svgRegex)) {
+			svgStuff = true
+		}
+
 		let newHeaders = new Headers(response.headers)
 		// newHeaders.set("Permissions-Policy", "interest-cohort=()")
-		newHeaders.set("Content-Security-Policy", `base-uri 'self' https://*.brycewray.com; connect-src 'self' https://*.brycewray.com https://*.cloudinary.com https://*.cloudflareinsights.com https://cloudflareinsights.com https://*.ytimg.com https://*.youtube-nocookie.com https://*.fosstodon.org https://*.mastodon.social https://*.mstdn.social; default-src 'self'; frame-ancestors 'self' https://*.brycewray.com https://*.youtube-nocookie.com; font-src 'self' https://*.brycewray.com data:; form-action 'self'; frame-src 'self' https://*.brycewray.com https://*.youtube-nocookie.com; img-src 'self' https://*.brycewray.com https://*.cloudinary.com https://*.ytimg.com https://*.youtube-nocookie.com https://*.twimg.com https://*.fosstodon.org https://*.mastodon.social https://*.mstdn.social https://*.google.com https://translate.googleapis.com data:; media-src 'self' https://*.brycewray.com https://*.cloudinary.com https://*.ytimg.com https://*.youtube-nocookie.com https://*.twimg.com https://*.fosstodon.org https://*.mastodon.social https://*.mstdn.social data:; object-src 'none'; script-src 'self' 'nonce-${nonce}' 'unsafe-inline' 'unsafe-eval'; script-src-elem 'self' 'nonce-${nonce}'; style-src 'self' https://*.brycewray.com https://*.youtube-nocookie.com data: https://*.google.com https://translate.googleapis.com 'nonce-${nonce}'; report-uri https://brycewray.report-uri.com/r/d/csp/reportOnly;`)
-		newHeaders.set("Report-To", "{'group':'default','max_age':31536000,'endpoints':[{'url':'https://brycewray.report-uri.com/a/d/g'}],'include_subdomains':true}")
+		newHeaders.set("Strict-Transport-Security", "max-age=63072000; includeSubDomains; preload")
+		newHeaders.set("X-Frame-Options", "SAMEORIGIN")
+		newHeaders.set("X-Content-Type-Options", "nosniff")
+		newHeaders.set("Referrer-Policy", "no-referrer, strict-origin-when-cross-origin")
+		if (ttl) {
+			newHeaders.set("Cache-Control", "public, max-age=" + ttl + ", immutable")
+			// newHeaders.set("CDN-Cache-Control", "public, max-age=" + ttl + ", immutable")
+		} else {
+			newHeaders.set("Cache-Control", "public, max-age=0, must-revalidate")
+			newHeaders.set("Content-Security-Policy", `base-uri 'self' https://*.brycewray.com; connect-src 'self' https://*.brycewray.com https://*.cloudinary.com https://*.cloudflareinsights.com https://cloudflareinsights.com https://*.ytimg.com https://*.youtube-nocookie.com https://*.fosstodon.org https://*.mastodon.social https://*.mstdn.social; default-src 'self'; frame-ancestors 'self' https://*.brycewray.com https://*.youtube-nocookie.com; font-src 'self' https://*.brycewray.com data:; form-action 'self'; frame-src 'self' https://*.brycewray.com https://*.youtube-nocookie.com; img-src 'self' https://*.brycewray.com https://*.cloudinary.com https://*.ytimg.com https://*.youtube-nocookie.com https://*.twimg.com https://*.fosstodon.org https://*.mastodon.social https://*.mstdn.social https://*.google.com https://translate.googleapis.com data:; media-src 'self' https://*.brycewray.com https://*.cloudinary.com https://*.ytimg.com https://*.youtube-nocookie.com https://*.twimg.com https://*.fosstodon.org https://*.mastodon.social https://*.mstdn.social data:; object-src 'none'; script-src 'self' 'nonce-${nonce}' 'unsafe-inline' 'unsafe-eval'; script-src-elem 'self' 'nonce-${nonce}'; style-src 'self' https://*.brycewray.com https://*.youtube-nocookie.com data: https://*.google.com https://translate.googleapis.com 'nonce-${nonce}'; report-uri https://brycewray.report-uri.com/r/d/csp/reportOnly;`)
+			newHeaders.set("Report-To", "{'group':'default','max_age':31536000,'endpoints':[{'url':'https://brycewray.report-uri.com/a/d/g'}],'include_subdomains':true}")
+			newHeaders.set("X-XSS-Protection", "1")
+		}
+		newHeaders.set("x-nonce-generator", "HIT")
+		if (jsStuff) {
+			newHeaders.set("Content-Type", "application/javascript; charset=utf-8")
+		}
+		if (svgStuff) {
+			newHeaders.set("Content-Type", "image/svg+xml; charset=utf-8")
+		}
 
 		return new Response(html, {
 			status: response.status,
