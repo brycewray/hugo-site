cd627e7257a66d16c246980db78f9611a6982990
Bryce Wray <bwray@wraytx.com>
2023-05-18 10:26:57 -0500

    Perhaps still easier to read

--- cfp-build.sh
+++ cfp-build.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 # # Always run with `bash cfp-build.sh` on CFP!
 
-echo "-----------------------"
-echo "-----------------------"
+echo "***********************"
 echo "Beginning shell script."
-echo "-----------------------"
-echo "-----------------------"
+echo "***********************"
 echo "                       "
 echo "-----------------------"
 echo "Cleaning 'public' and 'pagefind' dirs, just to be safe..."
 rm -rf public
 rm -rf static/_pagefind
+echo "                       "
 echo "-----------------------"
 echo "Running Hugo..."
 hugo --minify --gc
+echo "                       "
 echo "-----------------------"
 echo "Moving excerpted feed files..."
 mv public/posts/index.xml public/index-excerpts.xml
 mv public/posts/index.json public/index-excerpts.json
+echo "                       "
 echo "-----------------------"
 echo "Running search..."
 npm_config_yes=true npx pagefind@latest --source public
 echo "                       "
-echo "-----------------------"
-echo "-----------------------"
+echo "***********************"
 echo "Shell script complete!"
-echo "-----------------------"
-echo "-----------------------"
+echo "***********************"
 echo "                       "
