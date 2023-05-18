c4f4e10cb5865661331c552d1eb0c11a0badbd8e
Bryce Wray <bwray@wraytx.com>
2023-05-17 21:26:51 -0500

    `echo` cmds to delineate progress

--- cfp-build.sh
+++ cfp-build.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
+# # Always run with `bash cfp-build.sh` on CFP!
+
+echo "-----------------------"
+
+echo "Beginning shell script."
+
+echo "-----------------------"
+
+echo "Cleaning 'public' and 'pagefind' dirs, just to be safe..."
+
 rm -rf public
 
 rm -rf static/_pagefind
 
+echo "Running Hugo..."
+
 hugo --minify --gc
+
+echo "Moving excerpted feed files..."
 
 mv public/posts/index.xml public/index-excerpts.xml
 
 mv public/posts/index.json public/index-excerpts.json
 
+echo "Running search..."
+
 npm_config_yes=true npx pagefind@latest --source public --bundle-dir ../static/_pagefind
+
+echo "-----------------------"
+
+echo "Shell script complete!"
+
+echo "-----------------------"
