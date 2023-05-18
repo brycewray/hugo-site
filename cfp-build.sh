aba1bde7b93a66af86523cfca0519593dd8ca271
Bryce Wray <bwray@wraytx.com>
2023-05-17 20:44:43 -0500

    Try shell script method

---
+++ cfp-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -rf public
+
+rm -rf static/_pagefind
+
+hugo --gc --minify
+
+mv public/posts/index.xml public/index-excerpts.xml
+
+mv public/posts/index.json public/index-excerpts.json
+
+npm_config_yes=true npx pagefind@latest --source public --bundle-dir ../static/_pagefind
