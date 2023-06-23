1d594b13a2782fd88a9ff321c506fa311011d95c
Bryce Wray <bwray@wraytx.com>
2023-06-23 13:42:29 -0500

    [skip ci] Before adding OS-specific stuff (maybe)

--- startvers.sh
+++ startvers.sh
@@ -1,39 +1,49 @@
 #!/bin/sh
-echo "Checking requested versions..."
 HUGO_VERSION=0.114.1
 DART_SASS_VERSION=1.63.6
-VCHECK=$(hugo env)
-echo "\n-----\n"
-if grep -c "hugo v${HUGO_VERSION}" <<< ${VCHECK}
+
+SPACER="\n-----\n"
+
+echo "${SPACER}"
+
+echo "Checking requested versions...\n"
+
+if grep -q "hugo v${HUGO_VERSION}" <<< $(hugo env)
 then
   echo "Detected Hugo v.${HUGO_VERSION}!"
 else
   echo "Didn’t detect Hugo v.${HUGO_VERSION} -- installing it..."
-  echo " ----- "
+  echo "${SPACER}"
   wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_darwin-universal.tar.gz -O hugo_extended_${HUGO_VERSION}_darwin-universal.tar.gz
   tar -xvf hugo_extended_${HUGO_VERSION}_darwin-universal.tar.gz hugo
-  # rm -rf ../bin/hugo # just in case
+  rm -rf ../bin/hugo
   mv hugo ../bin
   xattr -dr com.apple.quarantine ../bin/hugo
-  rm -rf hugo_extended_${HUGO_VERSION}_darwin-universal.tar.gz
   hugo version
+  rm -rf hugo_extended_${HUGO_VERSION}_darwin-universal.tar.gz
 fi
-echo "\n-----\n"
-if grep -c "github.com/sass/dart-sass/compiler=\"${DART_SASS_VERSION}" <<< ${VCHECK}
+
+echo "${SPACER}"
+
+if grep -q "github.com/sass/dart-sass/compiler=\"${DART_SASS_VERSION}" <<< $(hugo env)
 then
   echo "Detected Dart Sass v.${DART_SASS_VERSION}!"
 else
   echo "Didn’t detect Dart Sass v.${DART_SASS_VERSION} -- installing it..."
-  echo " ----- "
+  echo "${SPACER}"
   curl -LJO https://github.com/sass/dart-sass/releases/download/${DART_SASS_VERSION}/dart-sass-${DART_SASS_VERSION}-macos-x64.tar.gz
   tar -xvf dart-sass-${DART_SASS_VERSION}-macos-x64.tar.gz
-  rm -rf ../bin/dart-sass # just in case
+  rm -rf ../bin/dart-sass
   mv dart-sass ../bin
   sass --embedded --version
   rm -rf dart-sass-${DART_SASS_VERSION}-macos-x64.tar.gz
 fi
-echo "\n-----\n"
+
+echo "${SPACER}"
+
 hugo env
-echo "\n-----\n"
+
+echo "${SPACER}"
+
 # now hand off to regular start script
 ./start.sh
