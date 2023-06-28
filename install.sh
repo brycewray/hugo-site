3097a2e311cb02b341af5105b8143adc0097ecff
Bryce Wray <bwray@wraytx.com>
2023-06-28 05:23:18 -0500

    Revert to original (macOS-only)

--- install.sh
+++ install.sh
@@ -1,25 +1,6 @@
 #!/bin/sh
 HUGO_VERSION=0.114.1
 DART_SASS_VERSION=1.63.6
-HUGO_ARCH_EXT='darwin-universal.tar.gz'
-# ^^^ HUGO_ARCH_EXT choices are... (Hugo Extended Version and 64-bit ONLY)
-# - 'darwin-universal.tar.gz' --- Universal binary for macOS, Apple Silicon OR Intel
-# - 'windows-amd64.zip' --- Windows on Intel/AMD CPUs (there's no ARM version for Windows)
-# - 'Linux-64bit.tar.gz' --- TAR archive, Linux on Intel (and, yes, it's a capital "L")
-# - 'linux-amd64.deb' --- Debian archive, Linux on AMD CPUs
-# - 'linux-amd64.tar.gz' --- TAR archive, Linux on AMD CPUs
-# - 'linux-arm64.deb' --- Debian archive, Linux ARM
-# - 'linux-arm64.tar.gz' --- TAR archive, Linux ARM
-SASS_ARCH_EXT='macos-x64'
-# ^^^ SASS_ARCH_EXT choices are...
-# - 'macos-arm64.tar.gz' --- macOS on Apple Silicon CPUs
-# - 'macos-x64.tar.gz' --- macOS on Intel CPUs
-# - 'windows-ia32.zip' --- Windows x86, 32-bit (no Windows ARM)
-# - 'windows-x64.zip' --- Windows x86, 64-bit (no Windows ARM)
-# - 'linux-arm.tar.gz' --- Linux ARM, 32-bit
-# - 'linux-arm64.tar.gz' --- Linux ARM, 64-bit
-# - 'linux-ia32.tar.gz' --- Linux x86, 32-bit
-# - 'linux-x64.tar.gz' --- Linux x86, 64-bit
 
 echo "Checking requested versions...\n"
 
