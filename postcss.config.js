8a51e19d4587c4393cb36fd5bf18039d940e88b1
Bryce Wray <bwray@wraytx.com>
2024-01-18 10:54:28 -0600

    Theming and config'ing for Lightning CSS via PostCSS

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+const postcssLightningcss = require("postcss-lightningcss");
+const postcssImport = require('postcss-import');
+
+module.exports = {
+	plugins: [
+    postcssImport({
+			path: "./themes/lcss/assets/css/",
+		}),
+		postcssLightningcss({
+			browsers: ">= .25%",
+			lightningcssOptions: {
+				drafts: {
+					nesting: true,
+				}
+			}
+		}),
+	]
+}
