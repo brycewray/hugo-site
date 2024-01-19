e607c05115d41703b9f2d7aaecbbbfef815eddb6
Bryce Wray <bwray@wraytx.com>
2024-01-19 11:18:47 -0600

    Comment-out TWCSS-related code

--- assets/js/mode-toggle.js
+++ assets/js/mode-toggle.js
@@ -46,11 +46,11 @@ function updateButton({ buttonEl, isDark }) {
 function updateThemeOnHtmlEl({ theme }) {
   document.querySelector("html").setAttribute("data-theme", theme)
 	// following part is for Tailwind CSS
-	if (theme === "dark") {
-		document.querySelector("html").classList.add('dark');
-	} else {
-		document.querySelector("html").classList.remove('dark');
-	}
+	// if (theme === "dark") {
+	// 	document.querySelector("html").classList.add('dark');
+	// } else {
+	// 	document.querySelector("html").classList.remove('dark');
+	// }
 	// end, part for Tailwind CSS
 }
 
