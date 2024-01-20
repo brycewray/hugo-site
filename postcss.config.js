e1c50c6803fcd639b204c5bb025a699c9ba96074
Bryce Wray <bwray@wraytx.com>
2024-01-19 21:31:11 -0600

    Remove the now-unneeded postcss-import

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,7 @@
-const postcssImport = require('postcss-import');
 const postcssLightningcss = require("postcss-lightningcss");
 
 module.exports = {
 	plugins: [
-    postcssImport({
-			path: "./themes/lcss/assets/css/",
-		}),
 		postcssLightningcss({
 			browsers: ">= 2%",
 			lightningcssOptions: {
