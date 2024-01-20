705d03e59412405b7bf37ccefd9477081920d3a5
Bryce Wray <bwray@wraytx.com>
2024-01-19 22:28:13 -0600

    Minify LCSS only in production

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,18 @@
 const postcssLightningcss = require("postcss-lightningcss");
+let minOpt
+
+if (process.env.HUGO_ENVIRONMENT === 'production') {
+	minOpt = true
+} else {
+	minOpt = false
+}
 
 module.exports = {
 	plugins: [
 		postcssLightningcss({
 			browsers: ">= 2%",
 			lightningcssOptions: {
+				minify: minOpt,
 				cssModules: false,
 				drafts: {
 					nesting: true,
