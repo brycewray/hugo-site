5d7728fca53b799b6596297352fb0d82cbb7ef99
Bryce Wray <bwray@wraytx.com>
2024-03-07 08:54:23 -0600

    Hugo 0.123.8

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 # --- for use with macOS and Linux/WSL
 
-HUGO_VERSION=0.123.7
+HUGO_VERSION=0.123.8
 HUGO_OS_ARCH='darwin-universal'
 # ^^^ choices for 'HUGO_OS_ARCH' (Extended Version only):
 # - 'darwin-universal' (macOS Universal Binary, Hugo 0.102.0 and up)
 # - 'linux-amd64' (Linux/WSL on x86-64)
 # - 'linux-arm64' (Linux/WSL on ARM-64)
 
-DARTSASS_VERSION=1.71.1
-DARTSASS_OS_ARCH='macos-arm64'
-# ^^^ choices for "DARTSASS_OS_ARCH":
-# - 'linux-arm64' (Linux/WSL on ARM-64)
-# - 'linux-x64' (Linux/WSL on x86-64)
-# - 'macos-arm64' (macOS on Apple Silicon)
-# - 'macos-x64' (macOS on Intel)
+# DARTSASS_VERSION=1.71.1
+# DARTSASS_OS_ARCH='macos-arm64'
+# # ^^^ choices for "DARTSASS_OS_ARCH":
+# # - 'linux-arm64' (Linux/WSL on ARM-64)
+# # - 'linux-x64' (Linux/WSL on x86-64)
+# # - 'macos-arm64' (macOS on Apple Silicon)
+# # - 'macos-x64' (macOS on Intel)
 
 echo "Checking requested versions...\n"
 
@@ -35,17 +35,17 @@ else
   rm -rf hugo_extended_${HUGO_VERSION}_${HUGO_OS_ARCH}.tar.gz
 fi
 
-if grep -q "github.com/sass/dart-sass/compiler=\"${DARTSASS_VERSION}" <<< $(hugo env)
-then
-  echo "Detected Dart Sass v.${DARTSASS_VERSION}!\n"
-else
-  echo "Failed to detect Dart Sass v.${DARTSASS_VERSION} --- installing it...\n"
-  curl -LJO https://github.com/sass/dart-sass/releases/download/${DARTSASS_VERSION}/dart-sass-${DARTSASS_VERSION}-${DARTSASS_OS_ARCH}.tar.gz
-  tar -xvf dart-sass-${DARTSASS_VERSION}-${DARTSASS_OS_ARCH}.tar.gz
-  rm -rf ../bin/dart-sass
-  mv dart-sass ../bin
-  sass --embedded --version
-  rm -rf dart-sass-${DARTSASS_VERSION}-${DARTSASS_OS_ARCH}.tar.gz
-fi
+# if grep -q "github.com/sass/dart-sass/compiler=\"${DARTSASS_VERSION}" <<< $(hugo env)
+# then
+#   echo "Detected Dart Sass v.${DARTSASS_VERSION}!\n"
+# else
+#   echo "Failed to detect Dart Sass v.${DARTSASS_VERSION} --- installing it...\n"
+#   curl -LJO https://github.com/sass/dart-sass/releases/download/${DARTSASS_VERSION}/dart-sass-${DARTSASS_VERSION}-${DARTSASS_OS_ARCH}.tar.gz
+#   tar -xvf dart-sass-${DARTSASS_VERSION}-${DARTSASS_OS_ARCH}.tar.gz
+#   rm -rf ../bin/dart-sass
+#   mv dart-sass ../bin
+#   sass --embedded --version
+#   rm -rf dart-sass-${DARTSASS_VERSION}-${DARTSASS_OS_ARCH}.tar.gz
+# fi
 
 hugo env
