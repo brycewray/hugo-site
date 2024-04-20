167c8d00e805c0c110f5e335284e7a1232bc9ab8
Bryce Wray <bwray@wraytx.com>
2024-04-20 13:28:23 -0500

    Try SCSS with Autoprefixer and PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,28 +1,27 @@
 // plugins for production only; Hugo will handle `@import`s
 
-const postcssPresetEnv =
-	require('postcss-preset-env')({
-		stage: 2,
-		features: {
-			'nesting-rules': true
-		}
-	})
+// const postcssPresetEnv =
+// 	require('postcss-preset-env')({
+// 		stage: 2,
+// 		// features: {
+// 		// 	'nesting-rules': true
+// 		// }
+// 	})
 const purgeCSS =
 	require('@fullhuman/postcss-purgecss')({
 		config: "./purgecss.config.js"
 	})
-// const autoPrefixer =
-// 	require('autoprefixer')({})
-const cssNano =
-	require('cssnano')({
-		preset: "default"
-	})
+const autoPrefixer =
+	require('autoprefixer')({})
+// const cssNano =
+// 	require('cssnano')({
+// 		preset: "default"
+// 	})
 
 module.exports = {
 	plugins: [
-		// autoPrefixer,
 		...process.env.HUGO_ENVIRONMENT === 'production'
-		? [postcssPresetEnv, purgeCSS, cssNano]
+		? [purgeCSS, autoPrefixer]
 		: []
 	]
 }
