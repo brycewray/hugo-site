f1f26a4cc82765de2bd3978ba5debded2cc66ca9
Bryce Wray <bwray@wraytx.com>
2024-04-20 14:59:07 -0500

    Dropping unused packages

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,11 @@
-// plugins for production only; Hugo will handle `@import`s
+// for production only
 
-// const postcssPresetEnv =
-// 	require('postcss-preset-env')({
-// 		stage: 2,
-// 		// features: {
-// 		// 	'nesting-rules': true
-// 		// }
-// 	})
 const purgeCSS =
 	require('@fullhuman/postcss-purgecss')({
 		config: "./purgecss.config.js"
 	})
 const autoPrefixer =
 	require('autoprefixer')({})
-// const cssNano =
-// 	require('cssnano')({
-// 		preset: "default"
-// 	})
 
 module.exports = {
 	plugins: [
