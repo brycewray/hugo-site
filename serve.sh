267923e44a51f3c562bca063cc953227c7ed6ae8
Bryce Wray <bwray@wraytx.com>
2024-05-20 17:13:56 -0500

    Back to Sass after encountering PostCSS oddities

--- serve.sh
+++ serve.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# MY_IP=$(<myip.txt)
-# npm_config_yes=true npx serve public -l tcp://${MY_IP}
-npm run testbuild
+MY_IP=$(<myip.txt)
+npm_config_yes=true npx serve public -l tcp://${MY_IP}
+# npm run testbuild
