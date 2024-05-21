21aaa7ffa13ae983085c8e31364739f904cc519d
Bryce Wray <bwray@wraytx.com>
2024-05-21 12:51:06 -0500

    Use npm cmds

--- pushbcfp.sh
+++ pushbcfp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sh ./push.sh
-sh ./build.sh
-sh ./deploy.sh # <-- this file purposely not public (CFP data)
-# npm run deploy
+# sh ./push.sh
+# sh ./build.sh
+# sh ./deploy.sh # <-- this file purposely not public (CFP data)
+npm run deploy
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MY_IP=$(<myip.txt)
-rm -rf public
-rm -rf static/pagefind && rm -rf static/_pagefind
-hugo server --port 3000 --bind=0.0.0.0 --baseURL=http://${MY_IP}:3000 --panicOnWarning --forceSyncStatic --gc
-# npm run start
+# rm -rf public
+# rm -rf static/pagefind && rm -rf static/_pagefind
+# hugo server --port 3000 --bind=0.0.0.0 --baseURL=http://${MY_IP}:3000 --panicOnWarning --forceSyncStatic --gc
+npm run start
