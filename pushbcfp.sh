ee436a954d6f0c97d86ffdfbb5d141a00e1985d9
Bryce Wray <bwray@wraytx.com>
2024-05-22 12:13:54 -0500

    Commented out too much

--- pushbcfp.sh
+++ pushbcfp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# sh ./push.sh
+sh ./push.sh # keep this with **both** Hugo-only **and** Hugo-with-npm
 # sh ./build.sh
 # sh ./deploy.sh # <-- this file purposely not public (CFP data)
 npm run deploy
