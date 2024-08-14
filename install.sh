f0bbd86f0de9f8ebe217a3301c7060a97260c6ff
Bryce Wray <bwray@wraytx.com>
2024-08-14 12:19:54 -0500

    Hugo 0.132.2

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # --- for use with macOS and Linux/WSL
 
-HUGO_VERSION=0.132.1
+HUGO_VERSION=0.132.2
 # https://github.com/gohugoio/hugo/releases/
 HUGO_OS_ARCH='darwin-universal'
 # ^^^ choices for 'HUGO_OS_ARCH' (Extended Version only):
