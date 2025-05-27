84456e8a24f7603f34702e3911d86b873e42d12d
Bryce Wray <bwray@wraytx.com>
2025-05-27 14:54:53 -0500

    Update for Workers purposes

--- pushbcfp.sh
+++ pushbcfp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sh ./push.sh # keep this with **both** Hugo-only **and** Hugo-with-npm
 sh ./build.sh
-sh ./deploy.sh # <-- this file purposely not public (CFP data)
+sh ./deploy-workers.sh # <-- since 2025-05-27 (https://discourse.gohugo.io/t/hugo-support-in-cloudflare-workers/54866)
 # npm run deploy
