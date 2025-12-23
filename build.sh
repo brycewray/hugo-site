2b094d1500de6926fb37a8c8de185a094585798b
Bryce Wray <bwray@wraytx.com>
2025-12-23 12:42:37 -0600

    [skip ci] Adjusting scripting for "new normal" with hvm and Hugo .pkg files

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+MY_PATH=$(<mypath.txt)
+HUGO_VERSION=$(<.hvm)
 rm -rf public
 rm -rf static/pagefind && rm -rf static/_pagefind # kill prev PF index, if any
-hugo --gc --minify --logLevel info
+${MY_PATH}/hvm/${HUGO_VERSION}/hugo --gc --minify --logLevel info
 mv public/posts/index.xml public/index-excerpts.xml
 mv public/posts/index.json public/index-excerpts.json
 npm_config_yes=true npx pagefind@latest --site public
--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 MY_IP=$(<myip.txt)
+MY_PATH=$(<mypath.txt)
+HUGO_VERSION=$(<.hvm)
 rm -rf public
 rm -rf static/pagefind && rm -rf static/_pagefind
-hugo server --port 3000 --bind=0.0.0.0 --baseURL=http://${MY_IP}:3000 --panicOnWarning --forceSyncStatic --gc
+${MY_PATH}/hvm/${HUGO_VERSION}/hugo server --port 3000 --bind=0.0.0.0 --baseURL=http://${MY_IP}:3000 --panicOnWarning --forceSyncStatic --gc
 # npm run start
