71db606891b055b5ee6bd1fad1a659744d2f859e
Bryce Wray <bwray@wraytx.com>
2026-03-28 16:55:03 -0500

    [skip ci] Comment out stuff for Dart Sass

--- install.sh
+++ install.sh
@@ -10,16 +10,16 @@ HVM_OS_ARCH='darwin-arm64'
 # - 'linux-amd64' (Linux/WSL on x86-64)
 # - 'linux-arm64' (Linux/WSL on ARM-64)
 
-DARTSASS_VERSION=1.98.0
-# https://github.com/sass/dart-sass/releases/
-DARTSASS_OS_ARCH='macos-arm64'
-# ^^^ choices for "DARTSASS_OS_ARCH":
-# - 'linux-arm64' (Linux/WSL on ARM-64)
-# - 'linux-x64' (Linux/WSL on x86-64)
-# - 'macos-arm64' (macOS on Apple Silicon)
-# - 'macos-x64' (macOS on Intel)
+# DARTSASS_VERSION=1.98.0
+# # https://github.com/sass/dart-sass/releases/
+# DARTSASS_OS_ARCH='macos-arm64'
+# # ^^^ choices for "DARTSASS_OS_ARCH":
+# # - 'linux-arm64' (Linux/WSL on ARM-64)
+# # - 'linux-x64' (Linux/WSL on x86-64)
+# # - 'macos-arm64' (macOS on Apple Silicon)
+# # - 'macos-x64' (macOS on Intel)
 
-echo "Checking requested versions...\n"
+echo "Checking requested version...\n"
 
 if grep -q "hvm ${HVM_VERSION}" <<< $(hvm version)
 then
@@ -38,15 +38,15 @@ else
   rm -rf hvm-${HVM_OS_ARCH}.tar.gz
 fi
 
-if grep -q "compilerVersion\": \"${DARTSASS_VERSION}" <<< $(sass --embedded --version)
-then
-  echo "Detected Dart Sass v.${DARTSASS_VERSION}!\n"
-else
-  echo "Failed to detect Dart Sass v.${DARTSASS_VERSION} --- installing it...\n"
-  curl -LJO https://github.com/sass/dart-sass/releases/download/${DARTSASS_VERSION}/dart-sass-${DARTSASS_VERSION}-${DARTSASS_OS_ARCH}.tar.gz
-  tar -xvf dart-sass-${DARTSASS_VERSION}-${DARTSASS_OS_ARCH}.tar.gz
-  rm -rf ../bin/dart-sass
-  mv dart-sass ../bin
-  sass --embedded --version
-  rm -rf dart-sass-${DARTSASS_VERSION}-${DARTSASS_OS_ARCH}.tar.gz
-fi
+# if grep -q "compilerVersion\": \"${DARTSASS_VERSION}" <<< $(sass --embedded --version)
+# then
+#   echo "Detected Dart Sass v.${DARTSASS_VERSION}!\n"
+# else
+#   echo "Failed to detect Dart Sass v.${DARTSASS_VERSION} --- installing it...\n"
+#   curl -LJO https://github.com/sass/dart-sass/releases/download/${DARTSASS_VERSION}/dart-sass-${DARTSASS_VERSION}-${DARTSASS_OS_ARCH}.tar.gz
+#   tar -xvf dart-sass-${DARTSASS_VERSION}-${DARTSASS_OS_ARCH}.tar.gz
+#   rm -rf ../bin/dart-sass
+#   mv dart-sass ../bin
+#   sass --embedded --version
+#   rm -rf dart-sass-${DARTSASS_VERSION}-${DARTSASS_OS_ARCH}.tar.gz
+# fi
