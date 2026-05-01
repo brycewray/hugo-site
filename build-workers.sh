9d3b255e4945efd329fd7404f30bada231075f5a
Bryce Wray <bwray@wraytx.com>
2026-05-01 16:08:12 -0500

    [skip ci] Updates for better processes

--- build-workers.sh
+++ build-workers.sh
@@ -4,6 +4,10 @@
 # Based on:
 # https://github.com/jmooring/hosting-cloudflare-worker/blob/main/build.sh
 # ...as of 2025-05-27
+#
+# The intent is to provide this script for `wrangler.jsonc` to access,
+# letting Cloudflare tool, wrangler, build to a Cloudflare Worker.
+#
 #------------------------------------------------------------------------------
 # @file
 # Builds a Hugo site hosted on a Cloudflare Worker.
@@ -16,34 +20,35 @@
 
 main() {
 
-  HUGO_VERSION=0.147.8
-  DART_SASS_VERSION=1.89.2
+  HUGO_VERSION=0.161.1
+  # DART_SASS_VERSION=1.89.2
 
   # set TZ --- https://discourse.gohugo.io/t/hugo-support-in-cloudflare-workers/54866/11
   echo "Setting the timezone to U.S. Central (CDT/CST)..."
-  export TZ=Chicago
+  export TZ=America/Chicago
 
-  # Install Dart Sass
-  echo "Installing Dart Sass v${DART_SASS_VERSION}..."
-  curl -LJO "https://github.com/sass/dart-sass/releases/download/${DART_SASS_VERSION}/dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
-  tar -xf "dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
-  cp -r dart-sass/ /opt/buildhome
-  rm -rf dart-sass*
+  # # Install Dart Sass
+  # echo "Installing Dart Sass v.${DART_SASS_VERSION}..."
+  # curl -LJO "https://github.com/sass/dart-sass/releases/download/${DART_SASS_VERSION}/dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
+  # tar -xf "dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
+  # cp -r dart-sass/ /opt/buildhome
+  # rm -rf dart-sass*
 
   # Install Hugo
-  echo "Installing Hugo v${HUGO_VERSION}..."
-  curl -LJO https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
-  tar -xf "hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
+  echo "Installing Hugo v.${HUGO_VERSION}..."
+  curl -LJO https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux-amd64.tar.gz
+  tar -xf "hugo_${HUGO_VERSION}_linux-amd64.tar.gz"
   cp hugo /opt/buildhome
-  rm LICENSE README.md hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
+  rm LICENSE README.md hugo_${HUGO_VERSION}_linux-amd64.tar.gz
 
   # Set PATH
   echo "Setting the PATH environment variable..."
-  export PATH=/opt/buildhome:/opt/buildhome/dart-sass:$PATH
+  # export PATH=/opt/buildhome:/opt/buildhome/dart-sass:$PATH
+  export PATH=/opt/buildhome:$PATH
 
   # Verify installed versions
   echo "Verifying installations..."
-  echo Dart Sass: "$(sass --version)"
+  # echo Dart Sass: "$(sass --version)"
   echo Go: "$(go version)"
   echo Hugo: "$(hugo version)"
   echo Node.js: "$(node --version)"
@@ -55,7 +60,6 @@ main() {
   git config core.quotepath false
 
   # Build the site.
-  # hugo --gc --minify
   echo "Building the site with Hugo..."
   hugo --gc --minify --logLevel info
 
